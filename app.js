a59bb6508e271dc0ba68f2a6f9e5071d54210df7
Underrooz <parmartarun.dev@gmail.com>
2019-01-18 20:06:30 +0530

    successfully added index page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const projectRoutes = require('./api/routes/project');
 const ideaRoutes = require('./api/routes/idea');
 const userRoutes = require('./api/routes/user');
-const indexRoutes = require('./api/routes/index');
+
 
 
 mongoose.connect('mongodb://Tarun:'+ process.env.MONGO_ATLAS_PW +'@cluster0-shard-00-00-gwous.mongodb.net:27017,cluster0-shard-00-01-gwous.mongodb.net:27017,cluster0-shard-00-02-gwous.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
@@ -31,7 +31,11 @@ app.set('view engine', 'ejs');
 //    return res.status(200).json({});
 //  }
 //});
+
 //app.use('/', indexRoutes);
+app.get('/', function(req, res) {
+  res.render('index');
+});
 app.use('/projects', projectRoutes);
 app.use('/ideas', ideaRoutes);
 app.use('/user', userRoutes);
